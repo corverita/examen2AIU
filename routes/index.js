e9ac8ed8e7775ed26e09e7c4dbe3553ac28e7a7b
Gerardo Jimenez Arguelles <33142689@uaz.edu.mx>
2020-06-24 20:29:28 -0500

    footer y acerca de

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/acerca_de', function(req, res, next) {
+  res.render('acerca_de', { title: 'Acerca de...'});
+});
+
 module.exports = router;
